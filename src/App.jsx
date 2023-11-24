85d805df5293ce4d635476aa34c7f122c512c523
Ajith-M-001 <ajithguru741@gmail.com>
2023-11-24 17:35:14 +0530

    vite setup and tailwind css install

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline bg-red-500 text-white text-center p-5">
+        Hello world!
+      </h1>
+    </div>
+  );
+};
+ 
+export default App;
