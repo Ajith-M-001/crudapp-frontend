5c30eb9fad59785f04297fc21a3c5391626dc425
Ajith-M-001 <ajithguru741@gmail.com>
2023-11-29 10:27:07 +0530

    added Homepage

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,5 +1,16 @@
 const Homepage = () => {
-  return <div>Homepage</div>;
+  return (
+    <div className="max-w-md mx-auto mt-8">
+      <h1 className="text-4xl text-center font-bold mb-5">Welcome Friends</h1>
+      <p className="mb-5 text-lg">
+        Today,we are building CRUD APP using MERN. A CRUD (Create, Read, Update,
+        Delete) application. This web app is built using the MERN (MongoDB,
+        Express.js, React, Node.js) stack, allowing users to manage and
+        manipulate data with ease. Explore the power of MERN technology in this
+        project.
+      </p>
+    </div>
+  );
 };
 
 export default Homepage;
