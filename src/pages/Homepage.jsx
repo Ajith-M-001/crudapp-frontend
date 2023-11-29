8dca95e3ea1030569d4b35bd4953c8a1230d8480
Ajith-M-001 <ajithguru741@gmail.com>
2023-11-29 10:51:25 +0530

    again changed homepage

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,13 +1,15 @@
 const Homepage = () => {
   return (
     <div className="max-w-lg mx-auto mt-8">
-      <h1 className="text-4xl text-center font-bold mb-5">Welcome Friends</h1>
+      <h1 className="text-4xl text-center font-bold mb-5">
+        Welcome Friends, i&rsquo;m Ajith
+      </h1>
       <p className="mb-5 text-lg">
-        Today,we are building CRUD APP using MERN. A CRUD (Create, Read, Update,
-        Delete) application. This web app is built using the MERN (MongoDB,
-        Express.js, React, Node.js) stack, allowing users to manage and
-        manipulate data with ease. Explore the power of MERN technology in this
-        project.
+        Today, we are building CRUD APP using MERN. A CRUD (Create, Read,
+        Update, Delete) application. This web app is built using the MERN
+        (MongoDB, Express.js, React, Node.js) stack, allowing users to manage
+        and manipulate data with ease. Explore the power of MERN technology in
+        this project.
       </p>
     </div>
   );
