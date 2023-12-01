fc36baff27c31bca7e011b4d18968950407445cc
Ajith-M-001 <ajithguru741@gmail.com>
2023-12-01 11:20:25 +0530

    changed Header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,12 +3,12 @@ import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <div className="bg-gray-700 text-gray-300 px-6 py-2">
-      <div className="container mx-auto flex justify-between items-center">
+      <div className="container mx-auto flex justify-between mt-7">
         <Link to={"/"}>
           <h1 className="text-3xl font-bold">CRUD-APP</h1>
         </Link>
         <nav className="space-x-6 text-2xl font-semibold">
-          <Link to={"/githubcode"} className="hover:text-white">
+          <Link to={"/githubcode"} className="hover:text-white text-lg">
             GitHub Code
           </Link>
           <Link to={"/add"} className="hover:text-white">
